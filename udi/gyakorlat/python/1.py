9fce7ac6e098e57fdb868831c83e197b3e49f815
yeymeap <bence3728@gmail.com>
2023-11-22 14:36:21 +0100

    exercises

--- udi/gyakorlat/python/1.py
+++
@@ -1,8 +0,0 @@
-age = int(input("Your age: "))
-if(age < 18):
-    print("You are not old enough to enter!")
-elif(age >= 18):
-    if(age >= 18 and age < 96):
-        print("Welcome!")
-    else:
-        print("I've never seen anyone above age 95, but welcome anyway!")
