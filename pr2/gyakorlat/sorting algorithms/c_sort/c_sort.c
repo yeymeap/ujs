d26f546f1e8f6e300476299c94ce87f7ffd7456b
yeymeap <bence3728@gmail.com>
2024-03-10 19:52:23 +0100

    debugger change

--- pr2/gyakorlat/sorting algorithms/c_sort/c_sort.c
+++ pr2/gyakorlat/sorting algorithms/c_sort/c_sort.c
@@ -4,55 +4,70 @@
 const int N = 20;
 int compare = 0, swap = 0;
 
-void PrintArray(int size, int a1[]){
-     for(int i = 0; i < size; i++){
+void PrintArray(int size, int a1[])
+{
+    for (int i = 0; i < size; i++)
+    {
         printf("%d ", a1[i]);
     }
 }
 
-void RandomArray(int size, int a1[]){
-    for(int i = 0; i < size; i++){
+void RandomArray(int size, int a1[])
+{
+    for (int i = 0; i < size; i++)
+    {
         a1[i] = rand() % 100 + 1;
     }
 }
 
-void SimpleSort(int size, int a1[]){
-     for(int i = 0; i < size; i++){
-        for(int j = i + 1; j < N; j++){
-            compare++;;
-            if(a1[i] > a1[j]){
-            int temp = a1[i];
-            a1[i] = a1[j];
-            a1[j] = temp;
-            swap++;
+void SimpleSort(int size, int a1[])
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            compare++;
+            ;
+            if (a1[i] > a1[j])
+            {
+                int temp = a1[i];
+                a1[i] = a1[j];
+                a1[j] = temp;
+                swap++;
             }
-            
         }
     }
 }
 
-void BubbleSort(int size, int a1[]){
-     for(int i = size; i > 0; i--){
-        for(int j = 0; j < i - 1 ; j++){
-            compare++;;
-            if(a1[j] > a1[j+1]){
+void BubbleSort(int size, int a1[])
+{
+    for (int i = size; i > 0; i--)
+    {
+        for (int j = 0; j < i - 1; j++)
+        {
+            compare++;
+            ;
+            if (a1[j] > a1[j + 1])
+            {
+                int temp = a1[j];
+                a1[j] = a1[j + 1];
+                a1[j + 1] = temp;
+                swap++;
+            }
+        }
+    }
+}
+
+void InsertionSort(int size, int a1[])
+{
+    for (int i = 1; i < size; i++)
+    {
+        int j = i - 1;
+        while (j >= 0 && a1[j] > a1[j + 1])
+        {
             int temp = a1[j];
-            a1[j] = a1[j+1];
+            a1[j] = a1[j + 1];
             a1[j + 1] = temp;
-            swap++;
-            }
-            
-        }
-    }
-}
-
-void InsertionSort(int size, int a1[]){
-    for (int i=1; i<size; i++) {
-        int j=i-1;        
-        while (j>=0 && a1[j]>a1[j+1]) {
-            int temp = a1[j];
-            a1[j] = a1[j+1];
-            a1[j+1] = temp;
             j--;
             swap++;
             compare++;
@@ -60,21 +75,22 @@ void InsertionSort(int size, int a1[]){
     }
 }
 
-void ChooseSort(int type, int a1[]){
-     switch(type){
-        case 1:
-            SimpleSort(N, a1);
-            break;
-        case 2:
-            BubbleSort(N, a1);
-            break;
-        case 3:
-            InsertionSort(N, a1);
-            break;
-        default:
+void ChooseSort(int type, int a1[])
+{
+    switch (type)
+    {
+    case 1:
+        SimpleSort(N, a1);
+        break;
+    case 2:
+        BubbleSort(N, a1);
+        break;
+    case 3:
+        InsertionSort(N, a1);
+        break;
+    default:
         printf("\nError during sort type choice!");
         exit(0);
-
     }
 }
 
@@ -90,5 +106,4 @@ int main()
     printf("\n");
     PrintArray(N, array1);
     printf("\ncomparisons: %d\nswaps: %d", compare, swap);
-    
 }
