1cd78b7742408ef1454f8aa6b5b01320096a051d
yeymeap <bence3728@gmail.com>
2024-03-12 12:16:27 +0100

    sorting

--- pr2/gyakorlat/sorting algorithms/c_sort/c_sort.c
+++ pr2/gyakorlat/sorting algorithms/c_sort/c_sort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 const int N = 20;
 int compare = 0, swap = 0;
 
@@ -73,6 +74,44 @@ void InsertionSort(int size, int a1[])
     }
 }
 
+void MaxSort(int size, int a1[])
+{
+    int max, temp;
+    for (int i = size; i >= 0; i--)
+    {
+        max = 0;
+        for (int j = 0; j <= i; j++)
+            if (a1[max] < a1[j])
+            {
+                max = j;
+                compare++;
+            }
+        temp = a1[max];
+        a1[max] = a1[i];
+        a1[i] = temp;
+        swap++;
+    }
+}
+
+void MinSort(int size, int a1[])
+{
+    int min, temp;
+    for (int i = size - 1; i > 0; i--)
+    {
+        min = i;
+        for (int j = 0; j <= i; j++)
+            if (a1[min] < a1[j])
+            {
+                min = j;
+                compare++;
+            }
+        temp = a1[min];
+        a1[min] = a1[i];
+        a1[i] = temp;
+        swap++;
+    }
+}
+
 void ChooseSort(int type, int a1[])
 {
     switch (type)
@@ -86,6 +125,12 @@ void ChooseSort(int type, int a1[])
     case 3:
         InsertionSort(N, a1);
         break;
+    case 4:
+        MaxSort(N, a1);
+        break;
+    case 5:
+        MinSort(N, a1);
+        break;
     default:
         printf("\nError during sort type choice!");
         return;
@@ -98,10 +143,10 @@ int main()
     srand(time(NULL));
     RandomArray(N, array1);
     PrintArray(N, array1);
-    printf("\n\nWhat sort do you want?\n\n1. Simple Sort\n2. Bubble Sort\n3. Insertion Sort\n\n");
-    while (scanf("%d", &sortType) != 1 || sortType < 1 || sortType > 3)
+    printf("\n\nWhat sort do you want?\n\n1. Simple Sort\n2. Bubble Sort\n3. Insertion Sort\n4. Max Sort\n5. Min Sort\n\n");
+    while (scanf("%d", &sortType) != 1 || sortType < 1 || sortType > 5)
     {
-        printf("Invalid input. Please enter a number between 1 and 3: ");
+        printf("Invalid input. Please enter a number between 1 and 5: ");
 
         while (getchar() != '\n')
             ;
