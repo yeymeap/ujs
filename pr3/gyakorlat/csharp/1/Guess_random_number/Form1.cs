1d98f8df269df8703131276118f0a1f7849160fb
yeymeap <bence3728@gmail.com>
2024-10-01 09:49:47 +0200

    csharp exercises

---
+++ pr3/gyakorlat/csharp/1/Guess_random_number/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guess_random_number
+{
+
+    public partial class Form1 : Form
+    {
+        private Random randgen = new Random();
+        private int gondoltSzam;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gondoltSzam = randgen.Next(1,51);
+            Console.WriteLine(gondoltSzam);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(textBox1.Text) > gondoltSzam)
+            {
+                label1.Text = ("Kisebb számra gondoltam");
+            }
+            else if (Convert.ToInt32(textBox1.Text) < gondoltSzam)
+            {
+                label1.Text = ("Nagyobb számra gondoltam");
+            }
+            else if (Convert.ToInt32(textBox1.Text) == gondoltSzam)
+            {
+                label1.Text = ("Eltaláltad, új számra gondoltam");
+                gondoltSzam = randgen.Next(1, 51);
+                Console.WriteLine(gondoltSzam);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ pr3/gyakorlat/csharp/1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int szamlalo = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (szamlalo == 0)
+            {
+                szamlalo++;
+                button1.Location = new Point(this.ClientSize.Width - button1.Width, 0);
+            }
+            else if (szamlalo == 1)
+            {
+                szamlalo++;
+                button1.Location = new Point(this.ClientSize.Width - button1.Width, this.ClientSize.Height - button1.Height);
+            }
+            else if (szamlalo == 2)
+            {
+                szamlalo++;
+                button1.Location = new Point(0, this.ClientSize.Height - button1.Height);
+            }
+            else if (szamlalo == 3)
+            {
+                szamlalo = 0;
+                button1.Location = new Point(0, 0);
+            }
+        }
+    }
+}
