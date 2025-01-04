ac275dea7a4f61f8275a8951044df76f764fcef6
yeymeap <bence3728@gmail.com>
2025-01-04 01:51:19 +0100

    game of life

--- 2/tsz/pr3/gyakorlat/csharp/2/Game_of_Life/Cells.cs
+++ 2/tsz/pr3/gyakorlat/csharp/2/Game_of_Life/Cells.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,8 +19,6 @@ namespace Game_of_Life
         }
         private CellState State = CellState.Dead;
         public const int SIZE = 20;
-        private bool isMouseDown = false;
-        
         public Cells()
         {
             this.Size = new Size(SIZE, SIZE);
@@ -45,6 +44,22 @@ namespace Game_of_Life
         public CellState GetState()
         {
             return this.State;
+        }
+        public void SetState(CellState state)
+        {
+            this.State = state;
+            switch (state)
+            {
+                case CellState.Alive:
+                    this.BackColor = Color.Black;
+                    break;
+                case CellState.Dead:
+                    this.BackColor = Color.White;
+                    break;
+                case CellState.Suspended:
+                    this.BackColor = Color.Gray;
+                    break;
+            }
         }
     }
 }
--- 2/tsz/pr3/gyakorlat/csharp/2/Game_of_Life/Form1.cs
+++ 2/tsz/pr3/gyakorlat/csharp/2/Game_of_Life/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,26 +14,36 @@ namespace Game_of_Life
 {
     public partial class Form1 : Form
     {
-        private Cells[,] cell = new Cells[30,30];
+        private Cells[,] cells;
+        private Timer timer;
+        private int rows;
+        private int cols;
+        private List<Cells> activeCells = new List<Cells>();
+        private bool gameState = false;
         public Form1()
         {
             InitializeComponent();
+            timer = new Timer();
+            timer.Interval = 500; // add slider to change speed
+            timer.Tick += new EventHandler(TimerTick);
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.Icon = Properties.Resources.icon;
             this.Text = "Game of Life";
-            int rows = 30;
-            int cols = 30;
+            rows = 30; // add grid size modification
+            cols = 30; // add grid size modification
+            cells = new Cells[rows, cols];
             int gridWidth = cols * Cells.SIZE;
             int gridHeight = rows * Cells.SIZE;
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    cell[i, j] = new Cells();
-                    cell[i, j].Location = new Point(Cells.SIZE * j, Cells.SIZE * i);
-                    cell[i, j].Click += new EventHandler(Clicked);
-                    this.Controls.Add(cell[i, j]);
+                    cells[i, j] = new Cells();
+                    cells[i, j].Location = new Point(Cells.SIZE * j, Cells.SIZE * i);
+                    cells[i, j].Click += new EventHandler(Clicked);
+                    this.Controls.Add(cells[i, j]);
                 }
             }
             GroupBox radioGroup = new GroupBox()
@@ -40,7 +52,7 @@ namespace Game_of_Life
                 Location = new Point(gridWidth + 10, 10),
                 Size = new Size(100, 100)
             };
-            string[] options = { "Game of Life", "Brian's Brain", "Seeds" };
+            string[] options = { "Game of Life", "Brian's Brain", "Seeds" }; // seperate the logic for each type of game
             int radioHeight = 20;
             for (int i = 0; i < options.Length; i++)
             {
@@ -48,67 +60,161 @@ namespace Game_of_Life
                 {
                     Text = options[i],
                     Location = new Point(10, 20 + i * radioHeight),
-                    AutoSize = true
+                    AutoSize = true,
+                    Checked = i == 0
                 };
                 radioGroup.Controls.Add(radio);
             }
-            string[] buttons = { "Start", "Stop", "Reset" };
+            this.Controls.Add(radioGroup);
+            string[] buttons = { "Start", "Stop", "Clear", "Reset" }; // combine buttons by making them change text and functionality based on state
+            int buttonWidth = 100;
             int buttonHeight = 30;
-            for(int i = 0; i < buttons.Length; i++)
+            EventHandler[] eventHandlers = { StartClicked, StopClicked,ClearClicked, ResetClicked };
+            for (int i = 0; i < buttons.Length; i++)
             {
                 Button button = new Button()
                 {
                     Text = buttons[i],
                     Location = new Point(gridWidth + 10, 120 + i * buttonHeight),
-                    Size = new Size(100, 30)
+                    Size = new Size(buttonWidth, buttonHeight)
                 };
-                if (i == 0)
-                {
-                    button.Click += new EventHandler(StartClicked);
-                }
-                else if(i == 1)
-                {
-                    button.Click += new EventHandler(StopClicked);
-                }
-                else if (i == 2)
-                {
-                    button.Click += new EventHandler(ResetClicked);
-                }
+                button.Click += eventHandlers[i];
                 this.Controls.Add(button);
             }
-            this.Controls.Add(radioGroup);  
-            this.ClientSize = new Size(gridWidth + radioGroup.Width + 20, Math.Max(gridHeight, radioGroup.Height) + 10);
+            this.ClientSize = new Size(gridWidth + radioGroup.Width + 20, Math.Max(gridHeight, radioGroup.Height + 10));
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.MinimizeBox = true;
             this.MaximizeBox = false;
         }
-        private List<Cells> activeCells = new List<Cells>();
-        private void Clicked(object sender,EventArgs e)
+        private void Clicked(object sender, EventArgs e)
         {
-            Cells c = (Cells)sender;
-            switch (c.GetState())
+            if (gameState == false)
             {
-                case Cells.CellState.Dead:
-                    c.SetAlive();
-                    activeCells.Add(c);
-                    break;
-                case Cells.CellState.Alive:
-                    c.SetDead();
-                    activeCells.Remove(c);
-                    break;
-                case Cells.CellState.Suspended:
-                    c.SetDead();
-                    activeCells.Remove(c);
-                    break;
+                Cells c = (Cells)sender;
+                switch (c.GetState())
+                {
+                    case Cells.CellState.Dead:
+                        c.SetAlive();
+                        activeCells.Add(c);
+                        break;
+                    case Cells.CellState.Alive:
+                        c.SetDead();
+                        activeCells.Remove(c);
+                        break;
+                    case Cells.CellState.Suspended:
+                        c.SetDead();
+                        activeCells.Remove(c);
+                        break;
+                }
             }
         }
-        private void ResetClicked(object sender, EventArgs e)
+        private void StartClicked(object sender, EventArgs e)
         {
-            foreach(Cells c in activeCells)
+            timer.Start();
+            //gameState = true;
+        }
+        private void StopClicked(object sender, EventArgs e)
+        {
+            timer.Stop();
+            //gameState = false;
+        }
+        private void ClearClicked(object _, EventArgs __)
+        {
+            foreach (Cells c in activeCells)
             {
                 c.SetDead();
             }
+            activeCells.Clear();
         }
-
+        private void ResetClicked(object _, EventArgs __)
+        {
+            
+        }
+        private int CountAliveNeighbours(int row, int col)
+        {
+            int aliveCount = 0;
+            for(int i = -1; i <= 1; i++)
+            {
+                for(int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        continue;
+                    }
+                    int neighbourRow = row + i;
+                    int neighbourCol = col + j;
+                    if (neighbourRow >= 0 && neighbourRow < rows && neighbourCol >= 0 && neighbourCol < cols)
+                    {
+                        Cells.CellState state = cells[neighbourRow, neighbourCol].GetState();
+                        if (state == Cells.CellState.Alive)
+                        {
+                            aliveCount++;
+                        }
+                    }
+                }
+            }
+            return aliveCount;
+        }
+        private void UpdateCells()
+        {
+            Cells[,] nextGrid = new Cells[rows, cols];
+            List<Cells> cellsToAdd = new List<Cells>();
+            List<Cells> cellsToRemove = new List<Cells>();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    nextGrid[i, j] = new Cells();
+                    int aliveNeighbours = CountAliveNeighbours(i, j);
+                    Cells.CellState state = cells[i, j].GetState();
+                    if (state == Cells.CellState.Alive)
+                    {
+                        if (aliveNeighbours < 2 || aliveNeighbours > 3)
+                        {
+                            nextGrid[i, j].SetDead();
+                            cellsToRemove.Add(cells[i, j]);
+                        }
+                        else
+                        {
+                            nextGrid[i, j].SetAlive();
+                            cellsToAdd.Add(cells[i, j]);
+                        }
+                    }
+                    else
+                    {
+                        if (aliveNeighbours == 3)
+                        {
+                            nextGrid[i, j].SetAlive();
+                            cellsToAdd.Add(cells[i, j]);
+                        }
+                        else
+                        {
+                            nextGrid[i, j].SetDead();
+                            cellsToRemove.Add(cells[i, j]);
+                        }
+                    }
+                }
+            }
+            foreach (Cells c in cellsToAdd)
+            {
+                activeCells.Add(c);
+            }
+            foreach (Cells c in cellsToRemove)
+            {
+                activeCells.Remove(c);
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    cells[i, j].SetState(nextGrid[i, j].GetState());
+                }
+            }
+        }
+        private void TimerTick(object sender, EventArgs e)
+        {
+            UpdateCells();// pause the game if all cells are dead
+            Console.WriteLine(activeCells.Count);
+        }
     }
 }
