97d4fd9953d66da2901c0bab0a028d667e0bc3e5
yeymeap <bence3728@gmail.com>
2025-09-22 17:12:35 +0200

    Color Filter in C#

---
+++ 3/tsz/pgg/gyakorlat/PGG/PGG/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace PGG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(670, 584);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Red";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonRGB_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(771, 584);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Green";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonRGB_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(872, 584);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Blue";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buttonRGB_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 584);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "RGB = ";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(670, 67);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PGG.Properties.Resources.cow_7200409_640;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 673);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 3/tsz/pgg/gyakorlat/PGG/PGG/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Xml;
+
+namespace PGG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonRGB_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null) return;
+
+            if(pictureBox2.Image != null)
+            {
+                pictureBox2.Image.Dispose();
+            }
+
+            pictureBox2.Image = ColorFilter((Bitmap)pictureBox1.Image, button.Text.ToLower());
+        }
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Color color = GetPixelAtClick((PictureBox)sender, e);
+            if (color != Color.Empty)
+            {
+                label1.Text = $"RGB = ({color.R}, {color.G}, {color.B})";
+            }
+
+        }
+        private Color GetPixelAtClick(PictureBox pictureBox, MouseEventArgs e)
+        {
+            if (pictureBox == null || pictureBox1.Image == null)
+            {
+                return Color.Empty;
+            }
+
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+
+            int x = e.X * bmp.Width / pictureBox1.Width;
+            int y = e.Y * bmp.Height / pictureBox1.Height;
+
+            if (x >= 0 && x < bmp.Width && y >= 0 && y < bmp.Height)
+            {
+                Color color = bmp.GetPixel(x, y);
+                return color;
+            }
+            return Color.Empty;
+        }
+
+        private Bitmap ColorFilter(Bitmap original, string channel)
+        {
+            if(original == null)
+            {
+                return null;
+            }
+
+            Bitmap filtered = new Bitmap(original.Width, original.Height);
+
+            for (int x = 0; x < original.Width; x++)
+            {
+                for (int y = 0; y < original.Height; y++)
+                {
+                    Color pixel = original.GetPixel(x, y);
+                    int r = 0;
+                    int g = 0;
+                    int b = 0;
+                    int value = 0;
+                    switch (channel)
+                    {
+                        case "red":
+                            r = pixel.R;
+                            value = pixel.R;
+                            break;
+                        case "green":
+                            value = pixel.G;
+                            g = pixel.G;
+                            break;
+                        case "blue":
+                            value = pixel.B;
+                            b = pixel.B;
+                            break;
+                    }
+                    //filtered.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    filtered.SetPixel(x, y, Color.FromArgb(value, value, value));
+                }
+            }
+            return filtered;
+        }
+    }
+}
